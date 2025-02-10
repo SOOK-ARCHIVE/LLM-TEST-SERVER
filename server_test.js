b4eac407ed8d2784a50463db566f3456ac5820e7
s0___0k <61587396+s0ooo0k@users.noreply.github.com>
2025-02-10 16:56:55 +0900

    [add] token 대비 server.js 수정, 작성 파일 server_test.js로 변경

--- server_test.js
+++ server_test.js
@@ -1,22 +1,57 @@
-require("dotenv").config();
+// https://expressjs.com/'
+//   const { body } = req;
+//   const { text } = body;
+// const { text } = req.body;
+// console.log(text);
+// https://www.together.ai/models
+// https://console.groq.com/docs/models
+// 1. 텍스트를 받아옴
+// 2-1. 이미지를 생성하는 프롬프트
+// llama-3-3-70b-free (together) -> 속도 측면
+// llama-guard-3-8b (groq) -> 안전하게 (이상한 표현)
+// 2-2. 그거에서 프롬프트만 JSON으로 추출
+// https://console.groq.com/docs/text-chat
+// - Mixtral performs best at generating JSON, followed by Gemma, then Llama
+// mixtral-8x7b-32768	(groq) 성능 1위
+// + gemma2-9b-it	(groq) 성능 2위 (그리고 나머지...)
+// + ... => 무료일경우에는 사용량문제고, 유료라면 단가?
+// 2-3. 그걸로 이미지를 생성
+// stable-diffusion-xl-base-1.0 (together)
+// 3-1. 설명을 생성하는 프롬프트
+// llama-3-3-70b-free (together)
+// 3-2. 그거에서 프롬프트만 추출
+// mixtral-8x7b-32768 (groq)
+// 3-3. 그걸로 thinking 사용해서 설명을 작성
+// DeepSeek-R1-Distill-Llama-70B-free (together)
+// 4. 그 결과를 { image: _, desc: _ }
+
+// https://www.together.ai/models/llama-3-3-70b-free
+
+require("dotenv").config(); // process.env
 
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
-
 const app = express();
 const port = 3000;
 
+app.use(cors()); // 미들웨어
+// 모두에게 오픈
+
 app.use(express.json());
-app.use(cors());
+
+app.get("/", (req, res) => {
+  // res.send("Hello World!");
+  res.send("Bye Earth");
+});
 
 app.post("/", async (req, res) => {
-  const { TOGETHER_API_KEY, GROQ_API_KEY } = process.env;
-  //   const TOGETHER_URL = "https://api.together.xyz/v1/chat/completions";
+  const { TOGETHER_API_KEY } = process.env;
+  const { GROQ_API_KEY } = process.env;
+  // const TOGETHER_URL = "https://api.together.xyz/v1/chat/completions";
   const TOGETHER_BASE_URL = "https://api.together.xyz";
   const GROQ_URL = "https://api.groq.com/openai/v1/chat/completions";
   const TURBO_MODEL = "meta-llama/Llama-3.3-70B-Instruct-Turbo-Free";
-  const GROQ_LLAMA_MODEL = "llama3-70b-8192";
   const FLUX_MODEL = "black-forest-labs/FLUX.1-schnell-Free";
   const MIXTRAL_MODEL = "mixtral-8x7b-32768";
   const DEEPSEEK_MODEL = "deepseek-ai/DeepSeek-R1-Distill-Llama-70B-free";
@@ -27,15 +62,12 @@ app.post("/", async (req, res) => {
     text,
     textForImage,
     apiKey,
-    jsonMode = false,
     max_tokens,
+    jsonMode = false,
   }) {
     const payload = {
       model,
     };
-    if (max_tokens) {
-      payload.max_tokens = max_tokens;
-    }
     if (text) {
       payload.messages = [
         {
@@ -43,6 +75,9 @@ app.post("/", async (req, res) => {
           content: text,
         },
       ];
+    }
+    if (max_tokens) {
+      payload.max_tokens = max_tokens;
     }
     if (textForImage) {
       payload.prompt = textForImage;
@@ -63,21 +98,17 @@ app.post("/", async (req, res) => {
   // 1. 텍스트를 받아옴
   const { text } = req.body;
 
-  // 2-1. 이미지를 생성하는 프롬프트
+  // 2-1. 이미지를 생성하는 프롬프트 -> 추출
   // llama-3-3-70b-free (together) -> 속도 측면
   const prompt = await callAI({
-    // url: `${TOGETHER_BASE_URL}/v1/chat/completions`,
-    // apiKey: TOGETHER_API_KEY,
-    // model: TURBO_MODEL,
-    url: GROQ_URL,
-    apiKey: GROQ_API_KEY,
-    model: GROQ_LLAMA_MODEL,
+    url: `${TOGETHER_BASE_URL}/v1/chat/completions`,
+    apiKey: TOGETHER_API_KEY,
+    model: TURBO_MODEL,
     // text,
-    text: `${text}의 위치나 스타일 정보를 바탕으로 술집(맥주 펍, 칵테일바, 와인바, 음식점 등등) 술집 추천에 어울리는 AI 이미지 생성을 위한 200자 이내의 영어 프롬프트를 작성해줘`,
+    text: `${text}의 요구 사항을 바탕으로 칵테일바, 맥주, 술집 등 우리 동네 술집 추천에 어울리는 AI 이미지 생성을 위한 200자 이내의 영어 프롬프트를 작성해줘`,
   }).then((res) => res.choices[0].message.content);
 
-  // 2-2. 그거에서 프롬프트만 JSON으로 추출
-  // mixtral-8x7b-32768	(groq)
+  // 2-2. 프롬프트 추출
   const promptJSON = await callAI({
     url: GROQ_URL,
     apiKey: GROQ_API_KEY,
@@ -87,8 +118,7 @@ app.post("/", async (req, res) => {
     jsonMode: true,
   }).then((res) => JSON.parse(res.choices[0].message.content).prompt);
 
-  // 2-3. 그걸로 이미지를 생성
-  // black-forest-labs/FLUX.1-schnell-Free (together)
+  // 2-3. 그걸로 이미지 생성
   const image = await callAI({
     url: `${TOGETHER_BASE_URL}/v1/images/generations`,
     apiKey: TOGETHER_API_KEY,
@@ -100,16 +130,13 @@ app.post("/", async (req, res) => {
   // 3-1. 설명을 생성하는 프롬프트
   // llama-3-3-70b-free (together)
   const prompt2 = await callAI({
-    // url: `${TOGETHER_BASE_URL}/v1/chat/completions`,
-    // apiKey: TOGETHER_API_KEY,
-    // model: TURBO_MODEL,
-    url: GROQ_URL,
-    apiKey: GROQ_API_KEY,
-    model: GROQ_LLAMA_MODEL,
+    url: `${TOGETHER_BASE_URL}/v1/chat/completions`,
+    apiKey: TOGETHER_API_KEY,
+    model: TURBO_MODEL,
     // text,
-    text: `${text}를 바탕으로 내가 원하는 스타일의 술집 추천(칵테일바, 와인바, 음식점 등)에 어울리는 설명 생성을 위한 200자 이내의 한글 프롬프트를 작성해줘`,
+    text: `${text}를 바탕으로 우리 동네 술집(맥주 펍, 칵테일바, 와인바, 일반 음식점 술집 등등) 추천에 어울리는 설명 생성을 위한 200자 이내의 한글 프롬프트를 작성해줘`,
   }).then((res) => res.choices[0].message.content);
-  console.log(prompt2);
+
   // 3-2. 그거에서 프롬프트만 추출
   // mixtral-8x7b-32768 (groq)
   const promptJSON2 = await callAI({
@@ -120,21 +147,19 @@ app.post("/", async (req, res) => {
     text: `${prompt2}에서 reasoning을 위해 작성된 200자 이내의 한글 프롬프트를 JSON Object로 prompt라는 key로 JSON string으로 ouput해줘`,
     jsonMode: true,
   }).then((res) => JSON.parse(res.choices[0].message.content).prompt);
-  console.log(promptJSON2);
+
   // 3-3. 그걸로 thinking 사용해서 설명을 작성
   // DeepSeek-R1-Distill-Llama-70B-free (together)
   const desc = await callAI({
     url: `${TOGETHER_BASE_URL}/v1/chat/completions`,
     apiKey: TOGETHER_API_KEY,
     model: DEEPSEEK_MODEL,
-    text: `${promptJSON2}를 기반으로 마크다운 문법 없이 평문으로 작성해주고 한글 결과물로 출력해줘. 엔터로 줄바꿈을 넣어줘.`,
+    text: promptJSON2,
     max_tokens: 2048,
   }).then((res) => res.choices[0].message.content.split("</think>")[1]);
-  console.log(desc);
-  //   desc = JSON.stringify(prompt);
-  //   console.log(image);
 
-  // 4. 그 결과를 { image: _, desc: _ }
+  // console.log(image);
+  // desc = JSON.stringify(promptJSON);
   res.json({
     image,
     desc,
